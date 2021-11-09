78c12e249f7596dae946222834bb1d9fd23c671f
Renato Delboni <renatonato_jr@hotmail.com>
2021-11-09 16:50:12 +0900

    not finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # CRUD
+
+  # READ
+  get '/tasks', to: 'tasks#index', as: :tasks
+
+  # CREATE
+  get '/tasks/new', to: 'tasks#new', as: :new_tasks
+  post '/tasks', to: 'tasks#create'
+
+  # READ (after new to avoid clash)
+  get '/tasks/:id', to: 'tasks#show', as: :task
+
+  # UPDATE
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_tasks
+  patch '/tasks/:id', to: 'tasks#update'
+
+  # DELETE
+  delete '/tasks/:id', to: 'tasks#destroy', as: :delete_tasks
 end
